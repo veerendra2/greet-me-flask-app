3fc9cfd8334432e29e80ba115f9bf6bbfbf80aff
veerendra2 <veerendra2@github.com>
2023-06-05 18:55:52 +0200

    Fix kustomize

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -6,13 +6,13 @@
 
 # for AMD64 / x86_64
 [ $(uname -m) = x86_64 ] \
-  && echo "[*] Install kind and kubectl binary" \
+  && echo "[*] Download kind and kubectl binary" \
   && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.19.0/kind-linux-amd64 \
   && curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
 # for ARM64
 [ $(uname -m) = aarch64 ] \
-  && echo "[*] Install kind and kubectl binary" \
+  && echo "[*] Download kind and kubectl binary" \
   && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.19.0/kind-linux-arm64 \
   && curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
 
